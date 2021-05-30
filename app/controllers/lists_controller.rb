b11201ce556a465a1fd7ad8bdc85b093383a88d0
Luciana Sugeta <lulusugeta@gmail.com>
2021-05-30 13:55:02 -0400

    read list controller, show list views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,4 +4,9 @@ class ListsController < ApplicationController
         @lists = List.all
         erb :'/lists/index'
     end
+
+    get '/lists/:id' do
+        @list = List.find(params[:id])
+        erb :'/lists/show'
+    end
 end
