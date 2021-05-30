19bc7268669cfaed8cbbfb93b355c5b19b92f510
Luciana Sugeta <lulusugeta@gmail.com>
2021-05-30 15:54:17 -0400

    List is now able to be updated with get and patch controllers and edit view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,8 +5,24 @@ class ListsController < ApplicationController
         erb :'/lists/index'
     end
 
+    get '/lists/new' do
+        erb :'/lists/new'
+    end
+
     get '/lists/:id' do
-        @list = List.find(params[:id])
+        @list = List.find_by_id(params[:id])
         erb :'/lists/show'
+    end
+
+    get '/lists/:id/edit' do
+        @list = List.find_by_id(params[:id])
+        erb :'/lists/edit'
+    end
+
+    patch '/lists/:id' do
+        @list = List.find_by_id(params[:id])
+        @list.update(name: params[:name], description: params[:description], to_do: params[:to_do], image: params[:image])
+        @list.save
+        redirect "/lists/#{@list.id}"
     end
 end
