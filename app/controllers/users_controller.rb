ba1fd915786bfb44d2af90e8e80c3c5e4ba6aa6b
Luciana Sugeta <lulusugeta@gmail.com>
2021-05-31 12:31:24 -0400

    signup user controller and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,17 @@
 class UsersController < ApplicationController
     
+    get '/signup' do
+        erb :'/users/signup'
+    end
+
+    post '/signup' do
+        user = User.new(name: params[:name], email: params[:email], password: params[:password])
+        if user.valid?
+            user.save
+            session[:user_id] = user.id
+            redirect '/lists'
+        else
+            redirect '/signup'
+        end
+    end
 end
