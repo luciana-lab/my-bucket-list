1357675cf18fd5c43f2d7e572f090476633cee55
Luciana Sugeta <lulusugeta@gmail.com>
2021-05-31 14:51:48 -0400

    private methods, list controller update and lists show views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,21 +20,49 @@ class ListsController < ApplicationController
     end
 
     get '/lists/:id/edit' do
+        redirect_if_not_login
+        
         @list = List.find_by_id(params[:id])
+        redirect_if_not_creator
         erb :'/lists/edit'
     end
 
     patch '/lists/:id' do
+
+        redirect_if_not_login
+
         @list = List.find_by_id(params[:id])
+        redirect_if_not_creator
+        if params[:name] == ""
+            redirect "/lists/#{params[:id]}/edit"
+        end
         @list.update(name: params[:name], description: params[:description], to_do: params[:to_do], image: params[:image])
         @list.save
         redirect "/lists/#{@list.id}"
+        
     end
 
     post '/lists/:id/delete' do
+        redirect_if_not_login
+
         @list = List.find_by_id(params[:id])
+        redirect_if_not_creator
         @list.delete
         redirect '/lists'
+    end
+
+    private
+
+    def redirect_if_not_login
+        if !logged_in?
+            redirect '/login'
+        end
+    end
+
+    def redirect_if_not_creator
+        if @list.user != current_user
+            redirect '/lists'
+        end
     end
 
 end
